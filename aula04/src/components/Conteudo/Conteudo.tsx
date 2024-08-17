314bf5ed1c9a91df7d59674c8efd7ecb1d167e29
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-16 22:08:23 -0300

    Componentes, cabecalho,conteudo,rodape e lista criados.

---
+++ aula04/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,7 @@
+export default function Conteudo(){
+    return(
+        <main>
+            <h2>Conteúdo</h2>
+        </main>
+    );
+}
---
+++ aula04/src/components/Lista/Lista.tsx
@@ -0,0 +1,8 @@
+export default function Lista(){
+    const nome = "Joaquim da Selva Xavier";
+    return(
+        <div>
+            <p>O nosso próximo candidato : {nome}</p>
+        </div>
+    );
+}
